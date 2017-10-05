a800ccce0c009f060f264e62d13ae577e225b2a7
Hydrophonix <hydrophonixfx@gmail.com>
2017-10-05 17:05:40 +0300

    updated JS1-2 ver. 1.1

--- JS1-2/js/main.js
+++ JS1-2/js/main.js
@@ -1,31 +1,33 @@
 var taskNumber = prompt('Enter the number of task', '1 or 2');
-if (taskNumber == 1) {
-  console.log('Result is ', Pow());
-} else {
-  if (taskNumber == 2) {
+
+switch (+taskNumber) {
+  case 1:
+    console.log('Result is ', Pow());
+    break;
+  case 2:
     alert('Welcome ' + Authorisation());
-  } else {
+    break;
+  default:
     alert('Invalid task number');
-  }
 }
 
 function Pow() {
-  var num = prompt('Enter the number you want to exponentiation', 'it must be an integer');
-  var exp = prompt('Enter the exponent', 'it must be an integer');
+  var num = prompt('Enter the number you want to exponentiation', 'it must be an integer and not infinite');
+  var exp = prompt('Enter the exponent', 'it must be an integer and not infinite');
   var n = Math.abs(exp);
   var result = 1;
-  if ((num % 1 == 0) & (n % 1 == 0)) {
+  if (isFinite(num + n) && (num % 1 == 0) && (n % 1 == 0)) {
     for (var i = 0; i < n; i++) {
       result *= num;
     }
 
     if (exp < 0) result = 1 / result;
     return result;
-  } else {
-    alert("Entered numbers isn't integer. Please enter correct numbers");
-    result = 'incorrect';
-    return result;
   }
+
+  alert("Entered numbers are infinite or isn't integer. Please enter correct numbers");
+  result = 'incorrect';
+  return result;
 }
 
 function Authorisation() {
