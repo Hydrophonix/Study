7a7d3a9e31ed51504370528cd7aaac2cfb3909e2
Hydrophonix <hydrophonixfx@gmail.com>
2017-10-13 15:35:53 +0300

    updated test2

--- Test/js/main.js
+++ Test/js/main.js
@@ -1,10 +1,10 @@
-var n = +prompt('ent num');
-function fetD(n) {
-  n =  ~~((n - ~~n) * 1e9) / 1e9;
-  return n;
-}
-
-alert(fetD(n));
+// var n = +prompt('ent num');
+// function fetD(n) {
+//   n =  ~~((n - ~~n) * 1e9) / 1e9;
+//   return n;
+// }
+//
+// alert(fetD(n));
 //
 // var str = prompt('ent text');
 // function fCuC(str) {
@@ -82,3 +82,165 @@ alert(fetD(n));
 // Erathosphen(arr, n);
 // console.log(arr);
 // console.log(Calculate(arr));
+//, 35, -55, 32, 8
+
+//                                                bssss
+
+// var arr = [-150, -100, 3, 10, 3, -5, 1, 2, -3, 4, 3, 2, -4, 6, -200, 5, 100];
+// function getMaxSubSum(arr) {
+//   var maxSum = 0;
+//   var sum2 = 0;
+//   var right = 0;
+//   var negative = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       if (negative >= 0 || maxSum == 0) {
+//         maxSum += arr[i];
+//         negative = 0;
+//       } else {
+//         if ((arr[i] + negative) > 0) {
+//           if ((maxSum + negative) > 0) {
+//             maxSum = maxSum + negative + arr[i];
+//             negative = 0;
+//             sum2 = 0;
+//           } else {
+//             maxSum = arr[i] + sum2;
+//             negative = 0;
+//           }
+//         } else {
+//           sum2 += arr[i];
+//           negative += arr[i];
+//         }
+//       }
+//     } else {
+//       negative += arr[i];
+//     }
+//
+//   }
+//
+//   return maxSum;
+// }
+//
+// console.log(getMaxSubSum(arr));
+
+// function getMaxSubSum(arr) {
+//   var sum = 0;
+//   var maxSum = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     maxSum = Math.max(sum, maxSum);
+//     if (sum < 0) sum = 0;
+//   }
+//
+//   return maxSum;
+// }
+//
+// console.log(getMaxSubSum([100, -2, 3]));
+// console.log(getMaxSubSum([1, 2, 3]));
+// console.log(getMaxSubSum([-1, -2, -3]));
+// console.log(getMaxSubSum([-1, 2, 3, -9]));
+// console.log(getMaxSubSum([-1, 2, 3, -9, 3, 11]));
+// console.log(getMaxSubSum([-2, -1, 1, 2]));
+
+//                                               Bsss
+
+// var obj = {
+//   className: 'open menu',
+// };
+// function addClass(obj, cls) {
+//   var names = obj.className.split(' ');
+//   for (var i = 0; i < names.length; i++) {
+//     if (names[i] == cls) return;
+//   }
+//
+//   names.push(cls);
+//   obj.className = names.join(' ');
+// }
+//
+// console.log(addClass(obj, 'new'), obj.className);
+// console.log(addClass(obj, 'open'), obj.className);
+// console.log(addClass(obj, 'me'), obj.className);
+//
+// console.log(obj.className);
+
+//                                        bsssss
+
+// function camelize(str) {
+//   var words = str.split('-');
+//   for (var i = 0; i < words.length; i++) {
+//     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+//   }
+//
+//   var result = words.join('');
+//   console.log(result);
+//   return result;
+// }
+//
+// console.log(camelize('background-color'));
+// camelize('list-style-image');
+// camelize('-webkit-transition');
+
+//                                      bsssssss
+
+// var obj = {
+//   className: 'open open wewer menu',
+// };
+// function removeClass(obj, cls) {
+//   var names = obj.className.split(' ');
+//   for (var i = 0; i < names.length; i++) {
+//     if (names[i] == cls) {
+//       names.splice(i, 1);
+//       i--;
+//     }
+//   }
+//
+//   obj.className = names.join(' ');
+// }
+//
+// console.log(removeClass(obj, 'open'), obj.className);
+// console.log(removeClass(obj, 'blabla'), obj.className);
+// console.log(removeClass(obj, 'menu'), obj.className);
+//
+// console.log(obj.className);
+
+//                                      bsssssss
+
+// var arr = [5, 3, 8, 1];
+// function filterRangeInPlace(arr, a, b) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] < a || arr[i] > b) {
+//       arr.splice(i--, 1);
+//     }
+//   }
+// }
+//
+// filterRangeInPlace(arr, 1, 4);
+// console.log(arr);
+
+//                                  bsgegsegse
+
+// function fnsort(a, b) {
+//   return b - a;
+// }
+//
+// var arr = [5, 2, 1, -10, 8];
+// arr.sort(fnsort);
+// console.log(arr);
+
+//                                    bbsbbaba
+/*jshint esversion: 6 */
+
+// var arr = ['Есть', 'жизнь', 'на', 'Марсе'];
+// var nArr = arr.map(obj => obj.length);
+// console.log(nArr);
+
+//                                        gwegsegseg
+
+var arr = [1, 2, 3, 4, 5];
+var total = [];
+var nArr = arr.reduce((first, second) => {
+  total.push(first);
+  return first + second;
+});
+total.push(nArr);
+console.log(total);
